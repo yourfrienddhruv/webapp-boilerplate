1e78f0615ec9a11f934a640c70626569c188548b
nikunj.thakkar <nikunj.thakkar@ishisystems.com>
2014-02-12 16:25:36 +0530

    File upload on div Click implemented.

--- webapp/static/js/angular/controllers/FileUploadController.js
+++ webapp/static/js/angular/controllers/FileUploadController.js
@@ -6,6 +6,7 @@
 
 function myFileUploadCtrl($scope, $upload) {
     $scope.onFileSelect = function ($files) {
+        console.log("Files :"+$files)
         //$files: an array of files selected, each file has name, size, and type.
         for (var i = 0; i < $files.length; i++) {
             var file = $files[i];
@@ -21,6 +22,27 @@ function myFileUploadCtrl($scope, $upload) {
             });
         }
     }
+    var callDialog = function(dialog, callback) {
+        dialog.addEventListener('change', function() {
+            var result = dialog.files;
+            console.log("File Name :"+result);
+            callback(result);
+        }, false);
+        dialog.click();
+    };
+    $scope.openFile = function(callback, multiple, acceptTypes) {
+        var dialog = document.createElement('input');
+        dialog.type = 'file';
+        if (multiple === true) {
+            dialog.multiple = 'multiple';
+        }
+        if (angular.isArray(acceptTypes)) {
+            dialog.accept = acceptTypes.join(',');
+        } else if (angular.isString(acceptTypes)) {
+            dialog.accept = acceptTypes;
+        }
+        callDialog(dialog, $scope.onFileSelect);
+    };
 };
 
 
